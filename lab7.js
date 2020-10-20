692e6f4d96703d042755dbcecb822c0ab9218988
Jessica Walker <jessicacrotty@gmail.com>
2020-10-20 01:58:00 -0400

    Completes lab with ran# & loops to create square.

---
+++ lab7.js
@@ -0,0 +1,13 @@
+/* Write a Node.js program that outputs a random square to the console. */
+
+var random = Math.ceil(Math.random() * 10);
+var square = "";
+
+for (var i = 0; i < random; i++) {
+    for (var j = 0; j < random; j++) {
+        square += "*";
+    }
+    square += "\n";
+}
+
+console.log(square);
