c54daeb34a21ae4aa44a193634faaab02eda6c52
Jessica Walker <jessicacrotty@gmail.com>
2020-10-21 23:55:48 -0400

    Updates lab 7 to include user input.

--- lab7.js
+++ lab7.js
@@ -4,17 +4,50 @@ Lab 7: Random Square
 Directions: Write a Node.js program that outputs a random square to the console. 
 */
 
+var square = "";    // string to hold square output
+
+// create input
+const readline = require("readline");
+const rlintf = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rlintf.question("Enter a whole number to create a square of that size: ", function(number) {
+
+    if (!isNaN(number) && number != "") {
+
+        for (var i = 0; i < number; i++) {
+            square += "\n";     // starts off adding space at the top so isn't up against prompt
+            for (var j = 0; j < number; j++) {
+                square += "*";
+            }
+        }
+
+        console.log(square);
+    }
+
+    else {
+        console.error("You did not enter a whole number. Exiting now.");
+    }
+
+    rlintf.close();
+});
+
+rlintf.on("close", function() {
+    process.exit(0);
+});
+
+// ==== random number generated version ====
+/*
+
 var random = Math.ceil(Math.random() * 10);
-var square = "";
-
-// switch from RGN to user input for square size
-
 for (var i = 0; i < random; i++) {
     for (var j = 0; j < random; j++) {
         square += "*";
     }
     square += "\n";
 }
-
-//console.log(square);
 process.stdout.write(square);
+
+*/
