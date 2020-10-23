7d7a78c19aa9415da2ac7d68f3660900ba8e7c40
Jessica Walker <jessicacrotty@gmail.com>
2020-10-23 02:12:51 -0400

    Adds directions for card app assignment.

---
+++ cards/6.js
@@ -0,0 +1,29 @@
+/* 
+Jessica Walker
+Homework 6: Cards
+*/
+
+/*
+Write a Node.js program that does the following:
+
+Save the program to a file named 6.js .
+
+Create a standard deck of 52 cards (e.g., "As", "2s", "3s", ... , "Td", "Jd", "Qd", "Kd"). Note that you must use T (not 10) to indicate the rank Ten.
+
+Deal ten unique, random cards to two players, alternating the deal.
+
+Compare the two five-card poker hands and determine the winner.
+
+Use the poker-evaluator (Links to an external site.) npm library to evaluate the two poker hands.
+Save the poker-evaluator npm module; generate the package.json and package-lock.json files to save the external library information.
+
+Submit 6.js, package.json, and package-lock.json to Canvas.
+
+You must format your output like below:
+
+    $ node 6.js
+    Hand 1: Qs 7c Kd 2h 8c  high card
+    Hand 2: Ad 6d 4h Jd Ah  one pair
+
+    Hand 2 wins!
+*/
