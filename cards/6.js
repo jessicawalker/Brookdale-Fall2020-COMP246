8364a734c12b51f9056a000da4e52877f218a853
Jessica Walker <jessicacrotty@gmail.com>
2020-10-23 20:21:58 -0400

    Cleans up comments and code in poker app.

--- cards/6.js
+++ cards/6.js
@@ -3,27 +3,18 @@ Jessica Walker
 Homework 6: Cards
 */
 
-/*
-Submit 6.js, package.json, and package-lock.json to Canvas.
-
-You must format your output like below:
-
-    $ node 6.js
-    Hand 1: Qs 7c Kd 2h 8c  high card
-    Hand 2: Ad 6d 4h Jd Ah  one pair
-
-    Hand 2 wins!
-*/
-
 const PokerEvaluator = require('poker-evaluator');
 const cardsToDeal = 10;
-var cardDeck = [];
-var player1Hand = [];
-var player2Hand = [];
+var hand1 = [];
+var hand2 = [];
+
+dealCards(createDeck());
+evaluateHands(hand1, hand2);
 
 function createDeck() {
     var value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"];
     var suit = ["c", "s", "h", "d"];
+    var cardDeck = [];
     var k = 0;  // tracks index of cardDeck array
 
     // concat value and suit to create card
@@ -39,43 +30,47 @@ function createDeck() {
 
 function dealCards(cardDeck) {
     var randomCard, dealtCard;
-    var j = 0, k = 0;
-    var currentCardDeck = cardDeck.concat();
+    var j = 0, k = 0;  // tracks indices of hands
 
     for (var i = 0; i < cardsToDeal; i++) {
-        randomCard = Math.floor(Math.random() * currentCardDeck.length);
-        dealtCard = currentCardDeck.splice(randomCard, 1).toString();
+
+        // pick a random card from deck and remove it from the deck
+        randomCard = Math.floor(Math.random() * cardDeck.length);
+        dealtCard = cardDeck.splice(randomCard, 1).toString();
+
+        // alternate dealing cards between players
         if (i % 2 == 0) {
-            player1Hand[j] = dealtCard;
+            hand1[j] = dealtCard;
             j++;
         }
         else {
-            player2Hand[k] = dealtCard;
+            hand2[k] = dealtCard;
             k++;
         }
     }
 }
 
-function evaluateHands(player1Hand, player2Hand) {
-    var player1 = PokerEvaluator.evalHand(player1Hand);
-    var player2 = PokerEvaluator.evalHand(player2Hand);
+function evaluateHands(hand1, hand2) {
+    // individual json objects from PokerEvaluator
+    var hand1Evaluated = PokerEvaluator.evalHand(hand1);
+    var hand2Evaluated = PokerEvaluator.evalHand(hand2);
 
-    console.log("Hand 1: " + player1Hand.toString().replace(/,/g, " ") + "  " + player1.handName);
-    console.log("Hand 2: " + player2Hand.toString().replace(/,/g, " ") + "  " + player2.handName);
+    // processes hand format to readable string text
+    var readableHand1 = hand1.toString().replace(/,/g, " ");
+    var readableHand2 = hand2.toString().replace(/,/g, " ");
+
+    // display hands and their hand types
+    console.log("Hand 1: " + readableHand1 + "  " + hand1Evaluated.handName);
+    console.log("Hand 2: " + readableHand2 + "  " + hand2Evaluated.handName);
     
-    if (player1.value > player2.value) {
+    // declare winner
+    if (hand1Evaluated.value > hand2Evaluated.value) {
         console.log("\nHand 1 wins!");
     }
-    else if (player1.value < player2.value) {
+    else if (hand1Evaluated.value < hand2Evaluated.value) {
         console.log("\nHand 2 wins!");
     }
     else {
         console.log("\nIt's a tie!");
     }
 }
-
-dealCards(createDeck());
-evaluateHands(player1Hand, player2Hand);
-
-//console.log(player1Hand);
-//console.log(player2Hand);
