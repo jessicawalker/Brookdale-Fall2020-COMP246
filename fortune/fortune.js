6c781b45856962ec1b16d6a4db3a1cf84a314f85
Jessica Walker <jessicacrotty@gmail.com>
2020-10-28 00:54:50 -0400

    Initializes fortune teller lab 9.

---
+++ fortune/fortune.js
@@ -0,0 +1,11 @@
+/* 
+Jessica Walker
+Lab 9: Fortune Teller
+
+Packages used:
+    nostra -- https://www.npmjs.com/package/nostra/v/0.0.6
+*/
+
+const nostra = require('nostra');
+
+var fortune = nostra.generate();
