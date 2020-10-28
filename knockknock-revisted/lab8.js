ba1e3c91b073e529d24880b324ad78afd9c4d1d6
Jessica Walker <jessicacrotty@gmail.com>
2020-10-28 01:02:30 -0400

    Adds copy of lab 8 for revised lab assignment.

---
+++ knockknock-revisted/lab8.js
@@ -0,0 +1,48 @@
+/* 
+Jessica Walker
+Lab 8: Knock, knock
+Directions: Write a Node.js program that gets a random knock-knock joke and displays it. 
+*/
+
+const joke = require('knock-knock-jokes');
+const fs = require('fs');
+
+var outputString = "";
+
+for (var i = 0; i < 3; i++) {
+    outputString += joke() + "@\n";
+}
+
+fs.writeFile("knockknock.txt", outputString, function(err) {
+    if (err) {
+        console.error(err);
+    }
+});
+
+fs.readFile("knockknock.txt", "utf8", checkReadResults);
+
+/*
+function checkReadResults(err, inputString) {
+    var inputArray = inputString.split("@");
+    for (var i = 0; i < inputArray.length; i++) {
+        console.log(inputArray[i] + "\n\n");
+    }
+}
+*/
+
+function checkReadResults(err, inputString) {
+    if(err) {
+         console.log(err);
+    } else {
+         displayJokes(inputString);
+    }
+}
+
+function displayJokes(inputString) {
+    var inputArray = inputString.split("@");
+
+
+    for(var i=0; i<inputArray.length; i++) {
+         console.log(inputArray[i] + "\n\n");
+    }
+}
