7d0fa4af719a071eb7263cdf33ac347b632686e1
Jessica Walker <jessicacrotty@gmail.com>
2020-11-11 01:48:19 -0500

    Installs express.

--- test_server/app.js
+++ test_server/app.js
@@ -5,10 +5,9 @@ app.get('/', function(req, res) {
     res.send('Hello World');
 });
 
-// did something wrong here, rewatch lecture about 7:40pm (~1.5 hours in) to see what I did wrong
 app.get('/love', function(req, res) {
     res.status(200).sendFile(__dirname + '/love.html');
-})
+});
 
 app.listen(4000);
 
