336a595901b06b50406941bddc9bb9baca20dda6
Jessica Walker <jessicacrotty@gmail.com>
2020-11-15 22:38:54 -0500

    Sets up initial files for HP EC assignment.

---
+++ HarryPotterEC/app/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const harryPotterSpells = require("harry-potter-spells");
+
+/*  DIRECTIONS:
+
+getSpells - A GET request that returns all the Harry Potter spells in JSON format.
+getSpell - A GET request that returns a single spell in JSON format.
+            Accept a name parameter and return the spell with that name. 
+            If the spell does not exist, return a JSON message stating so.
+addSpell - A POST request that adds a spell to the server. 
+            This request should accept three form fields: name, type, and effect. 
+            Return a JSON message indicating the status of the add request.
+
+Example:    {"name":"Silencio","type":"spell","effect":"Silences victim"}
+
+cURL Examples:
+
+$  # retrieve all spells
+$  curl -X GET -sS http://localhost:1234/getSpells  
+
+$  # retrieve only the Silencio spell
+$  curl -X GET -sS http://localhost:1234/getSpell?name=Silencio
+{"name":"Silencio","type":"spell","effect":"Silences victim"}
+
+$  # get a spell that does not exist
+$  curl -X GET -sS http://localhost:1234/getSpell?name=xyzxyz
+{"msg":"spell not found: xyzxyz"}
+
+$  # add a new spell
+$  curl --header "Content-Type: application/json" --request POST -sS --data '{ \"name\":\"Zap\",\"type\":\"spell\",\"effect\":\"Shocks victim.\" }' http://localhost:1234/addSpell
+{"msg":"spell added."}
+
+$  # get the spell that was just added
+$  curl -X GET -sS http://localhost:1234/getSpell?name=Zap
+{"name":"Zap","type":"spell","effect":"Shocks victim."}
+
+$  # try to add the same spell
+$  curl --header "Content-Type: application/json" --request POST -sS --data '{ \"name\":\"Zap\",\"type\":\"spell\",\"effect\":\"Shocks victim.\" }' http://localhost:1234/addSpell
+{"msg":"error - spell already exists!"}
+*/
