9e94800ae18917058fd974053d13db981dd24746
Jessica Walker <jessicacrotty@gmail.com>
2020-11-21 12:20:26 -0500

    Expands sorting fnc to cover all key types.

--- lab10_assignment/client/js/browse-movies.js
+++ lab10_assignment/client/js/browse-movies.js
@@ -16,19 +16,26 @@ $.ajax({
 });
 
 function sortByKey(key) {
-    return function(a, b) {
-        if (a[key] > b[key])
-            return 1;
-        else if (a[key] < b[key])
-            return -1;
 
-        return 0;
-    };
+    if (key == "ID" || key == "movieTitle" || key == "director") {
+        return function(a, b) {
+            if (a[key] > b[key])
+                return 1;
+            else if (a[key] < b[key])
+                return -1;
+
+            return 0;
+        };
+    } else if (key == "rank") {
+        return function(a, b) { return a[key] - b[key]; };
+    } else {
+        return function(a, b) { return b[key] - a[key]; };
+    }
 }
 
 function reload_js(src) {
-    $('script[src="' + src + '"]').remove();
-    $('<script>').attr('src', src).appendTo('head');
+    $("script[src=\"" + src + "\"]").remove();
+    $("<script>").attr("src", src).appendTo("head");
 };
 
 // inserts table row of data inside display table
@@ -44,7 +51,7 @@ function createMovieTable(movieData) {
         tableHTML += "<td>" + movieData[i].director + "</td>";
         tableHTML += "<td>" + movieData[i].rating + "</td>";
         tableHTML += "<td>" + movieData[i].users.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "</td>";
-        tableHTML += "<td class='delete_button'>" +
+        tableHTML += "<td class=\"delete_button\">" +
             "<button data-id=\"" +
             movieData[i].ID +
             "\">delete</button></td>";
@@ -64,7 +71,7 @@ function createMovieTable(movieData) {
             url: moviesURL + "/delete-record",
             type: "delete",
             data: { data: itemID },
-            success: $(this).closest("tr").animate({ opacity: '0' }, timeout),
+            success: $(this).closest("tr").animate({ opacity: "0" }, timeout),
             error: function(err) {
                 alert(err);
             }
